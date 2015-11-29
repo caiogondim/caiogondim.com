5f313ce0e46adb538b6bfdc51c57cfcfe00c0b19
Caio Gondim <caio.gondim@booking.com>
2015-11-29 12:58:33 +0100

    Add watch task to Metalsmith

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 
 var metalsmith = require('metalsmith')
 var layouts = require('metalsmith-layouts')
+const watch = require('metalsmith-watch')
 
 metalsmith(__dirname)
+    .use(watch({
+      paths: {
+        "${source}/**/*": true,
+        "layouts/*": "**/*",
+      }
+    }))
     .use(layouts({
       engine: 'handlebars'
     }))
