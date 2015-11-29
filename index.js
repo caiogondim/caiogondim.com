98ab11a489a37119a1fac61765e0bf4fe4e2c1fc
Caio Gondim <caio.gondim@booking.com>
2015-11-29 12:24:42 +0100

    Use handlebars template engine

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 var metalsmith = require('metalsmith')
+var layouts = require('metalsmith-layouts')
 
 metalsmith(__dirname)
+    .use(layouts({
+      engine: 'handlebars'
+    }))
     .destination('./dist')
     .build((error) => {
       if (error) throw error;
