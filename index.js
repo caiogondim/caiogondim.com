66691f639f4ea897075f86844b8f284c1a0059a5
Caio Gondim <me@caiogondim.com>
2015-12-04 00:21:55 +0100

    Add markdown support

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var metalsmith = require('metalsmith')
 var layouts = require('metalsmith-layouts')
 const watch = require('metalsmith-watch')
 const serve = require('metalsmith-serve')
+const markdown = require('metalsmith-markdown')
 
 metalsmith(__dirname)
     .use(serve({}))
+    .use(markdown())
     .use(watch({
       paths: {
         "${source}/**/*": true,
