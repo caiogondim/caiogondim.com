094b6b18a9cffb87e6c4bdb46aa175764aa6b66a
Caio Gondim <me@caiogondim.com>
2017-08-17 22:33:15 -0400

    Basic next.js infra

---
+++ .next/bundles/pages/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([4],{342:function(e,t,u){e.exports=u(343)},343:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(12),l=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return l.default.createElement("div",null,"Hello")}}},[342]);
+            return { page: comp.default }
+          })
+        
