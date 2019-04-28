a3143289423bf4ca08fc2bbb97e21145f8c566ee
Caio Gondim <me@caiogondim.com>
2019-04-27 22:01:50 -0400

    Collapse global styles into one component

--- components/shell.js
+++ components/shell.js
@@ -8,41 +8,44 @@ function Shell ({ children }) {
       <Head>
         <title>CAIO GONDIM</title>
         <link rel="icon" type="image/png" href="/static/logo.png" />
-        <Global styles={css`${superstylin.toString()}`} />
-        <Global styles={css`
-          body {
-            margin: calc((100vw / 25) * 1.563);
-            max-width: 100%;
-          }
+        <Global
+          styles={css`
+            ${superstylin.toString()}
 
-          section {
-            margin: 0 0 3.815rem 0;
-          }
+            body {
+              margin: calc((100vw / 25) * 1.563);
+              max-width: 100%;
+            }
 
-          section:last-child {
-            margin: 0;
-          }
+            section {
+              margin: 0 0 3.815rem 0;
+            }
 
-          a {
-            -webkit-tap-highlight-color: rgba(243, 243, 243, 0.5);
-          }
+            section:last-child {
+              margin: 0;
+            }
 
-          ::selection {
-            filter: invert(100%);
-            background-color: #343334;
-            color: white;
-          }
+            a {
+              -webkit-tap-highlight-color: rgba(243, 243, 243, 0.5);
+            }
 
-          main .from-back-forward-cache {
-            opacity: 1;
-            animation: none;
-          }
+            ::selection {
+              filter: invert(100%);
+              background-color: #343334;
+              color: white;
+            }
 
-          // Remove extra line on code blocks
-          code::after {
-            display: none;
-          }
-        `} />
+            main .from-back-forward-cache {
+              opacity: 1;
+              animation: none;
+            }
+
+            // Remove extra line on code blocks
+            code::after {
+              display: none;
+            }
+          `}
+        />
       </Head>
       {children}
     </div>
