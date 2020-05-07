607ad46621395f81173da096858b2759d4de57b2
Caio Gondim <me@caiogondim.com>
2020-05-06 22:46:54 -0400

    ADd 404 page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+import Shell from '../components/shell'
+import Header from '../components/header'
+import Content from '../components/content'
+
+class FourOhFour extends React.Component {
+  static getInitialProps ({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null
+    return { statusCode }
+  }
+
+  render () {
+    return (
+      <Shell>
+        <Header pageName="Error" />
+        <Content>
+          {this.props.statusCode && <h1>{this.props.statusCode}</h1>}
+          <p><Link href="/"><a>Go to index page</a></Link></p>
+        </Content>
+      </Shell>
+    )
+  }
+}
+
+FourOhFour.propTypes = {
+  statusCode: PropTypes.number
+}
+
+export default FourOhFour
