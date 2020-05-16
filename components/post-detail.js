fd0cfcc74911fd2f67591d79e4492e6a422df455
Caio Gondim <me@caiogondim.com>
2020-05-16 18:53:19 -0400

    Update packages

--- components/post-detail.js
+++ components/post-detail.js
@@ -58,7 +58,7 @@ PostDetail.propTypes = {
   meta: PropTypes.shape({
     title: PropTypes.string,
     link: PropTypes.string,
-    date: PropTypes.string
+    dateString: PropTypes.string
   })
 }
 
